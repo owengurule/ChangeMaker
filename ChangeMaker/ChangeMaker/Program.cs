4199cc2dbbd057d21d302a5e256f0c12d8377fc1
SeedPaths-User <lessdismissive@email.com>
2014-09-03 22:34:56 -0600

    complete

---
+++ ChangeMaker/ChangeMaker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ChangeMaker(3.18);
+            ChangeMaker(0.99);
+            ChangeMaker(12.93);
+
+            Console.ReadKey();
+        }
+        static void ChangeMaker(double amount)
+    {
+
+
+        List<string> coinList = new List<string>() { "Quarter", "Dime", "Nickle", "Penny"};
+
+
+
+        double Quarter = amount / .25;
+        double Dime = amount / .10;
+        double Nickle = amount / .05;
+        double Penny = amount / .01;
+
+        Console.WriteLine("$" + amount);
+        Console.WriteLine(Quarter + " " + "Quarter");
+        Console.WriteLine(Dime + " " + "Dime");
+        Console.WriteLine(Nickle + " " + "Nickle");
+        Console.WriteLine(Penny + " " + "Penny");
+
+
+            
+    }
+    }
+}
